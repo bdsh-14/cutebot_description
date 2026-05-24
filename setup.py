a2aa1f0ce5a164ec329d088547a6aea0b2e70a1c
bidisha <bdsh.14@gmail.com>
2026-05-23 18:35:40 -0600

    Fix setup and use urdf_tutorial in display

--- setup.py
+++ setup.py
@@ -1,4 +1,6 @@
 from setuptools import find_packages, setup
+import os
+from glob import glob
 
 package_name = 'cutebot_description'
 
@@ -10,6 +12,13 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        
+        # Install launch files
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.py')),
+        
+        # Install URDF/Xacro files
+        (os.path.join('share', package_name, 'urdf'), glob('urdf/*.xacro')),
+        (os.path.join('share', package_name, 'urdf'), glob('urdf/*.urdf')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
@@ -17,13 +26,38 @@ setup(
     maintainer_email='bb@todo.todo',
     description='TODO: Package description',
     license='TODO: License declaration',
-    extras_require={
-        'test': [
-            'pytest',
-        ],
-    },
+    tests_require=['pytest'],
     entry_points={
-        'console_scripts': [
-        ],
+        'console_scripts': [],
     },
 )
+
+# from setuptools import find_packages, setup
+
+# package_name = 'cutebot_description'
+
+# setup(
+#     name=package_name,
+#     version='0.0.0',
+#     packages=find_packages(exclude=['test']),
+#     data_files=[
+#         ('share/ament_index/resource_index/packages',
+#             ['resource/' + package_name]),
+#         ('share/' + package_name, ['package.xml']),
+#     ],
+#     install_requires=['setuptools'],
+#     zip_safe=True,
+#     maintainer='bb',
+#     maintainer_email='bb@todo.todo',
+#     description='TODO: Package description',
+#     license='TODO: License declaration',
+#     extras_require={
+#         'test': [
+#             'pytest',
+#         ],
+#     },
+#     entry_points={
+#         'console_scripts': [
+#         ],
+#     },
+# )
